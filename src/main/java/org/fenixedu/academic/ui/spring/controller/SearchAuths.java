287917fcb85ce0d5906e745c8bd446b0cb7dea06
danielfmpires <danielpires@campus.ul.pt>
2018-12-14 12:21:00 +0000

    fixed search page

--- src/main/java/org/fenixedu/academic/ui/spring/controller/SearchAuths.java
+++ src/main/java/org/fenixedu/academic/ui/spring/controller/SearchAuths.java
@@ -2,10 +2,18 @@ package org.fenixedu.academic.ui.spring.controller;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import org.fenixedu.academic.domain.AcademicProgram;
+import org.fenixedu.academic.domain.Degree;
 import org.fenixedu.academic.domain.accessControl.academicAdministration.AcademicAccessRule;
+import org.fenixedu.academic.domain.accessControl.academicAdministration.AcademicAccessRule.AcademicAccessTarget;
+import org.fenixedu.academic.domain.accessControl.academicAdministration.AcademicOperationType;
+import org.fenixedu.academic.domain.administrativeOffice.AdministrativeOffice;
+import org.fenixedu.academic.domain.phd.PhdProgram;
 import org.fenixedu.bennu.core.domain.Bennu;
 import org.fenixedu.bennu.core.domain.User;
 import org.fenixedu.bennu.spring.portal.SpringFunctionality;
@@ -14,6 +22,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import pt.ist.fenixframework.Atomic;
+import pt.ist.fenixframework.Atomic.TxMode;
 
 @Controller
 @SpringFunctionality(app = AcademicAdministrationSpringApplication.class, title = "label.search")
@@ -38,8 +50,18 @@ public class SearchAuths {
         final List<AcademicAccessRule> rules = getAuthorizations(user);
         final Map<String, String> users = getUsers();
 
+        final AcademicOperationType[] operations = AcademicOperationType.class.getEnumConstants();
+        final Set<AdministrativeOffice> offices = Bennu.getInstance().getAdministrativeOfficesSet();
+        final Set<Degree> degrees = Bennu.getInstance().getDegreesSet();
+        final Set<PhdProgram> phdPrograms = Bennu.getInstance().getPhdProgramsSet();
+
+        model.addAttribute("user", user);
         model.addAttribute("result", rules);
         model.addAttribute("users", users);
+        model.addAttribute("operations", operations);
+        model.addAttribute("offices", offices);
+        model.addAttribute("degrees", degrees);
+        model.addAttribute("phdPrograms", phdPrograms);
 
         return "authorizations/search";
     }
@@ -67,6 +89,88 @@ public class SearchAuths {
         });
 
         return users;
+    }
+
+    @RequestMapping(path = "addRule", method = RequestMethod.POST)
+    @ResponseBody
+    public String addRule(@RequestParam AcademicOperationType operation, @RequestParam User user) {
+
+        final Set<AcademicAccessTarget> targets = new HashSet<AcademicAccessTarget>();
+
+        final String id = grantRule(operation, user, targets);
+
+        return id;
+    }
+
+    @Atomic(mode = TxMode.WRITE)
+    private String grantRule(AcademicOperationType operation, User user, Set<AcademicAccessTarget> targets) {
+        final AcademicAccessRule rule = new AcademicAccessRule(operation, user.groupOf(), targets);
+        return rule.getExternalId();
+    }
+
+    @RequestMapping(path = "revoke", method = RequestMethod.POST)
+    public String revokeRule(Model model, @RequestParam AcademicAccessRule rule) {
+
+        revoke(rule);
+
+        return "authorizations/authorizationsByPerson";
+    }
+
+    @Atomic(mode = TxMode.WRITE)
+    private void revoke(AcademicAccessRule rule) {
+        rule.revoke();
+    }
+
+    @RequestMapping(path = "modifyOffice", method = RequestMethod.POST)
+    @ResponseBody
+    public String editAuthorizationOffice(Model model, @RequestParam AcademicAccessRule rule,
+            @RequestParam AdministrativeOffice scope, @RequestParam String action) {
+
+        final Set<AdministrativeOffice> offices = rule.getOfficeSet();
+
+        if (action.equals("add")) {
+            addOffice(rule, scope, offices);
+        } else {
+            removeOffice(rule, scope, offices);
+        }
+
+        return scope.getExternalId();
+    }
+
+    @Atomic(mode = TxMode.WRITE)
+    private void removeOffice(AcademicAccessRule rule, AdministrativeOffice office, Set<AdministrativeOffice> offices) {
+        offices.remove(office);
+    }
+
+    @Atomic(mode = TxMode.WRITE)
+    private void addOffice(AcademicAccessRule rule, AdministrativeOffice office, Set<AdministrativeOffice> offices) {
+        offices.add(office);
+    }
+
+    @RequestMapping(path = "modifyProgram", method = RequestMethod.POST)
+    @ResponseBody
+    public String editAuthorizationProgram(Model model, @RequestParam AcademicAccessRule rule,
+            @RequestParam AcademicProgram scope, @RequestParam String action) {
+
+        final Set<AcademicProgram> programs = rule.getProgramSet();
+
+        if (action.equals("add")) {
+            addProgram(rule, scope, programs);
+        } else {
+            removeProgram(rule, scope, programs);
+        }
+
+        return scope.getExternalId();
+    }
+
+    @Atomic(mode = TxMode.WRITE)
+    private void removeProgram(AcademicAccessRule rule, AcademicProgram program, Set<AcademicProgram> programs) {
+        programs.remove(program);
+    }
+
+    @Atomic(mode = TxMode.WRITE)
+    private void addProgram(AcademicAccessRule rule, AcademicProgram program, Set<AcademicProgram> programs) {
+        programs.add(program);
     }
 
 }
